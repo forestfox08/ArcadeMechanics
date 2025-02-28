c57f67d88260f26e896a30695cb5c443a576db37
forestfox08 <j.rdb08@hotmail.com>
2025-02-28 10:36:14 +0100

    commit1

---
+++ Assets/Movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public Rigidbody body;
+
+    public float MovementSpeed;
+
+    private Vector2 MoveInput;
+    private Vector2 MouseInput;
+
+    public float MouseSensitivty = 2f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
