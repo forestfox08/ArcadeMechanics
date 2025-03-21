2e244e0073f078e977b9295de6aab6f8bbb2616f
Adem-ma <37601@ma-web.nl>
2025-03-21 10:53:45 +0100

    movenwordmoveeee

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,15 +4,47 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    float moveSpeed = 5;
+    float sensitivity = 5;
+    public GameObject Camera;
+    public GameObject cameraFocus;
     void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * moveSpeed * Time.deltaTime;
+            
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position += -transform.forward * moveSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += -transform.right * moveSpeed * Time.deltaTime;
+
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += transform.right * moveSpeed * Time.deltaTime;
+
+        }
+
+        //float mouseX = Input.GetAxis("Mouse X");
+        //float mouseY = Input.GetAxis("Mouse Y");
+
+        //transform.Rotate(0, mouseX * sensitivity, 0);
+        //cameraFocus.transform.Rotate(-mouseY * sensitivity, 0, 0);
+
     }
 }
